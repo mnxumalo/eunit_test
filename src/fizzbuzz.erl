e705525f9cf067634653ef951a16831721798ef6
Mthulisi Nxumalo <mnxumalo@gmail.com>
2020-03-26 20:26:00 +0200

    Added fizzbuzz process

--- src/fizzbuzz.erl
+++ src/fizzbuzz.erl
@@ -10,7 +10,19 @@
 -author("mthulisi").
 
 %% API
--export([analyze/1]).
+-export([analyze/1, loop/0, calculate/2, shutdown/1, start/0]).
+
+start() -> spawn(fun() -> loop() end).
+
+calculate(Pid, N) ->
+  io:format("My Pid is: ~p~n", [self()]),
+  Pid ! {analyze, self(), N},
+  receive
+    Result -> Result
+  end.
+
+shutdown(Pid) ->
+  Pid ! shutdown.
 
 analyze(N) ->
   if
@@ -18,4 +30,14 @@ analyze(N) ->
     N rem 3 == 0 -> fizz;
     N rem 5 == 0 -> buzz;
     true -> N
+  end.
+
+loop() ->
+  receive
+    {analyze, CallerId, Value} ->
+      CallerId ! analyze(Value),
+      loop();
+    shutdown ->
+      io:format("Shutting down..."),
+      ok
   end.
